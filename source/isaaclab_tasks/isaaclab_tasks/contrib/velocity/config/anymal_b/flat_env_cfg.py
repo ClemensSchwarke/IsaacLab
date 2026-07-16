f395908c4645c7650e0cc5a4be88d46736211e4e
ClemensSchwarke <clemens.schwarke@gmail.com>
2026-07-16 15:53:14 +0200

    unify contrib configs

--- source/isaaclab_tasks/isaaclab_tasks/contrib/velocity/config/anymal_b/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/contrib/velocity/config/anymal_b/flat_env_cfg.py
@@ -37,34 +37,32 @@ class AnymalBFlatEnvCfg(AnymalBRoughEnvCfg):
     sim: SimulationCfg = SimulationCfg(physics=PhysicsCfg())
 
     def __post_init__(self):
-        # post init of parent
         super().__post_init__()
 
-        # override rewards
+        # scene
+        self.scene.terrain.terrain_type = "plane"
+        self.scene.terrain.terrain_generator = None
+        self.scene.height_scanner = None
+        # observations
+        self.observations.policy.height_scan = None
+        # rewards
         self.rewards.flat_orientation_l2.weight = -5.0
         self.rewards.dof_torques_l2.weight = -2.5e-5
         self.rewards.feet_air_time.weight = 0.5
-        # change terrain to flat
-        self.scene.terrain.terrain_type = "plane"
-        self.scene.terrain.terrain_generator = None
-        # no height scan
-        self.scene.height_scanner = None
-        self.observations.policy.height_scan = None
-        # no terrain curriculum
+        # curriculum
         self.curriculum.terrain_levels = None
 
 
 @configclass
 class AnymalBFlatEnvCfg_PLAY(AnymalBFlatEnvCfg):
-    def __post_init__(self) -> None:
-        # post init of parent
+    def __post_init__(self):
         super().__post_init__()
 
-        # make a smaller scene for play
+        # scene
         self.scene.num_envs = 50
         self.scene.env_spacing = 2.5
-        # disable randomization for play
+        # observations
         self.observations.policy.enable_corruption = False
-        # remove random pushing event
+        # events
         self.events.base_external_force_torque = None
         self.events.push_robot = None
